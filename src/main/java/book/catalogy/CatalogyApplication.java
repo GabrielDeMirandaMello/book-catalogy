2e9894f9f966469fa1910900a984d0449fa93f2c
Gabriel Mello <gabriel.mmello@sptech.school>
2024-01-24 11:50:27 -0300

    algumas configurações e detalhes do projeto

---
+++ src/main/java/book/catalogy/CatalogyApplication.java
@@ -0,0 +1,13 @@
+package book.catalogy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/book/catalogy/CatalogyApplicationTests.java
@@ -0,0 +1,13 @@
+package book.catalogy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
